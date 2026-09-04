c947838446cedf8aff2507c36b6e73b91673666a
gilvane.user <gilvane.rodrigues@sigacred.com.br>
2026-09-04 09:52:13 -0300

    prof Marines

--- content/pages.js
+++ content/pages.js
@@ -5,7 +5,7 @@
 window.JOURNAL_GROUPS = [
   {
     id: "1A",
-    label: "1A",
+    label: "Culturas no Paraná - 1A",
     title: "Turma 1A",
     pages: [
       { title: "Publicação da turma 1A", file: "content/1.jpeg", type: "image" }
@@ -14,7 +14,7 @@ window.JOURNAL_GROUPS = [
   {
     id: "2A-2C",
     label: "Literatura - 2A",
-    title: "Turma 2A",
+    title: "Turma 2A - Literatura",
     pages: [
       { title: "Literatura 2A", file: "content/Janaina-2a/Literatura 2a.jpeg", type: "image" },
       { title: "Literatura 2A", file: "content/Janaina-2a/Literatura1 2a.jpeg", type: "image" },
@@ -27,7 +27,7 @@ window.JOURNAL_GROUPS = [
   {
     id: "2C",
     label: "Literatura - 2C",
-    title: "Turma 2C",
+    title: "Turma 2C - Literatura",
     pages: [
       { title: "Literatura", file: "content/Fernanda/Fernanda1.jpeg", type: "image" },
       { title: "Literatura", file: "content/Fernanda/Fernanda2.jpeg", type: "image" },
@@ -42,13 +42,72 @@ window.JOURNAL_GROUPS = [
   {
     id: "1A",
     label: "Geografia do Paraná - 1A",
-    title: "Turma 1A",
+    title: "Turma 1A - Geografia do Paraná",
     pages: [
       { title: "Geografia do Paraná", file: "content/Sergiana/Sergiana.png", type: "image" },
       { title: "Geografia do Paraná", file: "content/Sergiana/Sergiana1.png", type: "image" },
       { title: "Geografia do Paraná", file: "content/Sergiana/Sergiana2.png", type: "image" },
       { title: "Geografia do Paraná", file: "content/Sergiana/Sergiana3.png", type: "image" }
 
+    ]
+  },
+  {
+    id: "HISTORIA-PR-1A",
+    label: "História do Paraná - 1A",
+    title: "Turma 1A - História do Paraná",
+    pages: [
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem1.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem2.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem3.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem4.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem5.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem6.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem7.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem8.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem9.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem10.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem11.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem12.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem13.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem14.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem15.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem16.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem17.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem18.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem19.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem20.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem21.jpg", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem22.png", type: "image" },
+      { title: "História do Paraná", file: "content/Marines/História do Paraná - 1A/imagem23.png", type: "image" }
+    ]
+  },
+  {
+    id: "SOCIOLOGIA-GOVERNO-CIDADANIA-2A",
+    label: "Sociologia, Governo e Cidadania - 2A",
+    title: "Turma 2A - Sociologia, Governo e Cidadania",
+    pages: [
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem1.png", type: "image" },
+      { title: "Papa Francisco", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem2.png", type: "image" },
+      { title: "Albert Einstein", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem3.png", type: "image" },
+      { title: "Machado de Assis", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem4.png", type: "image" },
+      { title: "Djamila Ribeiro", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem5.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem6.png", type: "image" },
+      { title: "Lélia Gonzalez", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem7.png", type: "image" },
+      { title: "Sueli Carneiro", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem8.png", type: "image" },
+      { title: "Movimento dos Sem Terra (MST)", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem9.png", type: "image" },
+      { title: "Literatura e Cultura", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem10.png", type: "image" },
+      { title: "Literatura e Cultura", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem11.png", type: "image" },
+      { title: "Movimento Estudantil", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem12.png", type: "image" },
+      { title: "Movimento dos indígenas", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem13.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem14.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem15.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem16.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem17.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem18.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem19.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem20.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem21.png", type: "image" },
+      { title: "Sociologia, Governo e Cidadania", file: "content/Marines/Socilogia, Governo e Cidadania - 2A/imagem22.png", type: "image" }
     ]
   }
 ];
